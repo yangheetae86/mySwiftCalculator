020de910410a4f4b85deef45304d9ce37dc1c453
yang <heroparkjisung2gmail.com>
2020-04-15 19:00:27 +0900

    new

--- myCalculator/ContentView.swift
+++ myCalculator/ContentView.swift
@@ -10,7 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            HStack {
+                ForEach(["7","8","9","X"], id: \.self) { button in
+                    Text(button)
+                }
+                .font(.system(size: 32))
+            }
+        }
     }
 }
 
