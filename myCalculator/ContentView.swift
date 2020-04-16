366c052ef567405371af2bc34eb1e3a5d97af851
yang <heroparkjisung2gmail.com>
2020-04-16 18:50:41 +0900

    new

--- myCalculator/ContentView.swift
+++ myCalculator/ContentView.swift
@@ -9,15 +9,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    let buttons: Array = [["7","8","9","X"],["4","5","6","-"],["1","2","3","+"],["0",".",".","="]]
+    
     var body: some View {
-        VStack {
-            HStack {
-                ForEach(["7","8","9","X"], id: \.self) { button in
-                    Text(button)
+        ZStack(alignment: .bottom) {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 12) {
+                HStack {
+                    Spacer()
+                    Text("42")
+                        .foregroundColor(.white)
+                        .font(.system(size: 64))
+                        .padding()
+                }.padding()
+                
+                ForEach(buttons, id: \.self) { row in
+                    HStack(spacing: 12) {
+                        ForEach(row, id: \.self) { button in
+                            Text(button)
+                                .font(.system(size: 32))
+                                .frame(width: self.buttonWidth(), height: self.buttonWidth())
+                                .foregroundColor(.white)
+                                .background(Color.yellow)
+                                .cornerRadius(40)
+
+                        }
+                    }
                 }
-                .font(.system(size: 32))
-            }
+            }.padding()
         }
+    }
+    func buttonWidth() -> CGFloat {
+        (UIScreen.main.bounds.width - 5 * 12) / 4
     }
 }
 
